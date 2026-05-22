7c58e3970b1d6b380e333a527613514df76b4930
b3m2a1 <b3m2a1@gmail.com>
2026-05-22 18:59:44 -0500

    Update pipeline.py

--- cmcc_reactions/pipeline.py
+++ cmcc_reactions/pipeline.py
@@ -841,6 +841,7 @@ def generate_from_product_library(
         product_file = os.path.basename(product_file)
         if input_file is None:
             input_file = product_file
+        print("?", input_file)
         curdir = os.getcwd()
         try:
             os.chdir(targ_dir)
