4e9a585d6bbaee986a48dd36023b90e5a7d07cd9
b3m2a1 <b3m2a1@gmail.com>
2026-05-23 00:43:09 -0500

    Update pipeline.py

--- cmcc_reactions/pipeline.py
+++ cmcc_reactions/pipeline.py
@@ -637,7 +637,7 @@ def run_fmrds(optimizer,
 
 def run_internal_fmrds(optimizer,
                        internal_selector='dihedrals',
-                       magnitude=(-200, -100, -50, 50, 100, 200),
+                       magnitude=(-200, 200),
                        max_internals=10,
                        verbose=True,
                        pool=None,
