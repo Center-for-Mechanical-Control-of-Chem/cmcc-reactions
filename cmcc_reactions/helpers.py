52acc5685bae3a1945127ad03c1d69a230a9360d
b3m2a1 <b3m2a1@gmail.com>
2026-06-10 22:44:08 -0500

    Update helpers.py

--- cmcc_reactions/helpers.py
+++ cmcc_reactions/helpers.py
@@ -1,4 +1,5 @@
 import os
+import stat
 
 def configure_cli(target_dir='.', templates_dir=None):
     container = os.environ['SINGULARITY_CONTAINER'] # error out early
@@ -6,15 +7,18 @@ def configure_cli(target_dir='.', templates_dir=None):
     script = os.path.join(target_dir, f'run_{name}_sif.sh')
 
     from McUtils.ExternalPrograms import SLURMClient
-    if templates_dir is None:
-        templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
     SLURMClient.create_server_package(target_dir, overwrite=True)
 
+    if templates_dir is None:
+        templates_dir = os.path.join(os.path.dirname(__file__), 'templates')
     with open(os.path.join(templates_dir, 'container_runner.sh'), 'r') as f:
         src = f.read()
         src = src.replace('`CONTAINER_PATH`', container)
         with open(script, 'w+') as dest:
             dest.write(src)
+
+    st = os.stat(script)
+    os.chmod(script, st.st_mode | stat.S_IEXEC)
 
     return script
 
