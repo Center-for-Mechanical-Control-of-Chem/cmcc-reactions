3f7d871e8cbd86d50afc3a4bde9f943a05104397
b3m2a1 <b3m2a1@gmail.com>
2026-06-15 11:38:38 -0500

    Add sbatch runner to container configuration

--- cmcc_reactions/helpers.py
+++ cmcc_reactions/helpers.py
@@ -4,7 +4,8 @@ import stat
 def configure_cli(target_dir='.', templates_dir=None):
     container = os.environ['SINGULARITY_CONTAINER'] # error out early
     name = os.path.splitext(os.path.basename(container))[0]
-    script = os.path.join(target_dir, f'run_{name}_sif.sh')
+    script_name = f'run_{name}_sif.sh'
+    script = os.path.join(target_dir,script_name)
 
     from McUtils.ExternalPrograms import SLURMClient
     SLURMClient.create_server_package(target_dir, overwrite=True)
@@ -15,6 +16,14 @@ def configure_cli(target_dir='.', templates_dir=None):
         src = f.read()
         src = src.replace('`CONTAINER_PATH`', container)
         with open(script, 'w+') as dest:
+            dest.write(src)
+
+    sbatch_name = f'{name}_sif_python.sh'
+    sbatch_path = os.path.join(target_dir, sbatch_name)
+    with open(os.path.join(templates_dir, 'container_sbatch.sh'), 'r') as f:
+        src = f.read()
+        src = src.replace('`CONTAINER_SCRIPT_NAME`', script_name)
+        with open(sbatch_path, 'w+') as dest:
             dest.write(src)
 
     st = os.stat(script)
